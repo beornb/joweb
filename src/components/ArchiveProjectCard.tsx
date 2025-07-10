9d9cc96b11de7f51aee24d2489ad562e101f868a
Beorn Bishop <beorn@morpher.com>
2025-07-10 08:50:48 +0200

    Archive Projects Back Button

--- src/components/ArchiveProjectCard.tsx
+++ src/components/ArchiveProjectCard.tsx
@@ -10,12 +10,11 @@ interface ArchiveProjectCarsProps {
 
 export default function ArchiveProjectCard({ image, text, link }: ArchiveProjectCarsProps) {
   return (
-        <Link target='_blank' href={`/CaseStudies/${link}.html`} >
-
-    <div className="items-center justify-center mb-6   p-0">
-                <Image src={`/CaseStudies/${image}.png`} alt="arrow icon" width={700} height={120} />
-      <p className="text-gray-700 text-left text-base mt-4">{text}<span className='text-[var(--rose-gold-fg-mid)]'>..Read more</span></p>
-    </div>
+    <Link target='_top' href={`/CaseStudies/${link}.html`} >
+      <div className="items-center justify-center mb-6   p-0">
+        <Image src={`/CaseStudies/${image}.png`} alt="arrow icon" width={700} height={120} />
+        <p className="text-gray-700 text-left text-base mt-4">{text}<span className='text-[var(--rose-gold-fg-mid)]'>..Read more</span></p>
+      </div>
     </Link>
   );
 }
